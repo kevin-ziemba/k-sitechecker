1848288c5cbad5fd5c52c55c9df62aafb11efd2a
Kevin Ziemba <ziembakevin@gmail.com>
2019-07-17 21:34:43 -0400

    retrieve status from websites or raise error

--- website_checker.py
+++ website_checker.py
@@ -1,1 +1,8 @@
 import requests
+
+def url_status_code(url):
+    try:
+        r = requests.get(url)
+        return str(r.status_code)
+    except:
+        return "Failed to establish a new connection"
