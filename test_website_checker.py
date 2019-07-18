9144b3b1e41c53a56c6640579c49479f49b2d240
Kevin Ziemba <ziembakevin@gmail.com>
2019-07-17 21:35:06 -0400

    unit tests for website_checker.py

---
+++ test_website_checker.py
@@ -0,0 +1,19 @@
+import website_checker
+import unittest
+
+class TestWebsiteChecker(unittest.TestCase):
+
+    def test_https(self):
+        self.assertEqual(website_checker.url_status_code('https://www.google.ca/'), "200")
+
+    def test_http(self):
+        self.assertEqual(website_checker.url_status_code('http://espn.com'), "200")
+
+    def test_fake_address(self):
+        self.assertNotEqual(website_checker.url_status_code('ht://espn.com'), "200")
+        self.assertNotEqual(website_checker.url_status_code('http://espn'), "200")
+        self.assertNotEqual(website_checker.url_status_code('http://thisdoesnotexist1klhbndswjlflhfk3423.com'), "200")
+
+
+if __name__ == '__main__':
+    unittest.main()
